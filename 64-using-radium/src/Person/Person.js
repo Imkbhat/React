f6185d3537bdbf99d10000b2a19fbeba6c7a8567
Kiran B S <kbs71190@gmail.com>
2019-02-18 16:43:03 +0530

    Add files via upload

---
+++ 64-using-radium/src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Radium from 'radium';
+import './Person.css';
+
+const person  = (props) => {
+    //return <p>I'm a Person! and I am {Math.floor(Math.random()*30)} Years Old!</p>
+    return (
+    
+    <div className="Person">
+        <p onClick={props.click}>I'm {props.name} and I am {props.age} Years Old!</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+    )
+};
+
+export default Radium(person);
