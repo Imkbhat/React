38331d5cd851711c570e695d0b19eaa859bf05c6
Kiran B S <kbs71190@gmail.com>
2019-02-20 13:39:31 +0530

    Add files via upload
    1. Need to run sudo  npm  install --save prop-types
    2. import PropTypes from  'prop-types'
    
    //Validating Props Key val pair
    className.propTypes
    3. Person.propTypes = {
        click: PropTypes.func,
        name: PropTypes.string,
        age: PropTypes.number,
        changed: PropTypes.func 
    };
    //If age is passed as Num, below error  wll be observed in console.
    Warning: Failed prop type: Invalid prop `age` of type `string` supplied to `Person`, expected `number`.
        in Person (at withClass.js:17)

---
+++ 100-Validating Props/src/components/Persons/Person/Person.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './Person.css';
+import withClass from '../../../hoc/withClass';
+import Aux from '../../../hoc/Aux';
+
+/*const person = ( props ) => {
+    return (
+        <div className={classes.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
+
+export default person;*/
+//changing functional  component to Class component
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[Person*.js] Inside Constructor',  props);
+      }
+  
+      componentWillMount() {
+        console.log('[Person*.js] Inside component willmont');
+      }
+  
+      componentDidMount() {
+        console.log('[Person*.js] Inside componentDidMount');
+      }
+
+    render () {
+        console.log('[Person*.js] Inside render()');
+        return (
+            <Aux>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </Aux>
+        ) 
+    }
+};
+
+//Validating Props Key val pair
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func 
+};
+
+export default withClass(Person,  classes.Person);
