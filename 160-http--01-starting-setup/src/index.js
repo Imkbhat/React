6136762ead36c594bb6199364d80ba2434e40539
Kiran B S <kbs71190@gmail.com>
2019-02-28 14:04:43 +0530

    Add files via upload

---
+++ 160-http--01-starting-setup/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+//global handler
+axios.interceptors.request.use(request => {
+    console.log(request);
+    //Edit request Config.
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    //Edit request Config.
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+})
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
